391f64665d4b2cfc0c6abdc34749bd90cb736703
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-07-27 02:07:00 -0700

    01_02

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,9 +1,15 @@
 import "./menu.scss"
 
-const Menu = () => {
+const Menu = ({ menuOpen, setMenuOpen }) => {
     return (
-        <div className="menu">
-            
+        <div className={"menu "+(menuOpen && "active")}>
+            <ul>
+                <li onClick={()=>setMenuOpen(false)}><a href="#intro">Home</a></li>
+                <li onClick={()=>setMenuOpen(false)}><a href="#portofolio">Portofolio</a></li>
+                <li onClick={()=>setMenuOpen(false)}><a href="#works">Works</a></li>
+                <li onClick={()=>setMenuOpen(false)}><a href="#testimonials">Testimonials</a></li>
+                <li onClick={()=>setMenuOpen(false)}><a href="#contact">Contact</a></li>
+            </ul>
         </div>
     )
 }
