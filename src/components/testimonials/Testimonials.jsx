81d2754e60e815f0a2707fcdf67102f22a8ecb18
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-07-27 20:01:15 -0700

    02_01

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -3,7 +3,15 @@ import "./testimonials.scss"
 const Testimonials = () => {
     return (
         <div className="testimonials" id="testimonials">
-            testimonials
+            <h1>testimonials</h1>
+            <div className="container">
+                <div className="card">
+                    <div className="top">   
+                    </div>
+                    <div className="center"></div>
+                    <div className="bottom"></div>
+                </div>
+            </div>
         </div>
     )
 }
